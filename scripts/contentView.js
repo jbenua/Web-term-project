e569c9dbb17c6247c53fad56b4ac89faeef26e23
Eugenia <harumm.scarumm@gmail.com>
2015-06-22 23:29:41 +0300

    WTF

--- scripts/contentView.js
+++ scripts/contentView.js
@@ -1,15 +1,20 @@
         function expanddiv(id){
-            opacity=parseOpacity();
-            raise_opacity(opacity ? opacity : 0);
+            var a=parseOpacity();
+            var opacity = a ? a : 0
             var currentEl=document.getElementById(id);
             for (var i=0, elems = document.getElementsByTagName('p'), len = elems.length; i<len; i++) {
                     removeClass(elems[i], 'active');
             }
             if (currentEl) {
+                raise_opacity(opacity);
                 currentEl.className += ' active';
                 par.style.display="none";
             }
-            else par.style.display="block";
+            else
+            {
+                decrease_opacity(opacity);
+                par.style.display="block";
+            }
         };
         function parseOpacity()
         {
@@ -21,13 +26,31 @@
             var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
             element.className = element.className.replace(reg,' ');
         }
+        function decrease_opacity(cur_opacity)
+        {
+            setTimeout(function(){
+                if (cur_opacity > 0) {
+                    set_opacity(cur_opacity);
+                }
+                else{
+                    set_opacity(0);
+                    return;
+                    }
+                cur_opacity-=0.005;
+                decrease_opacity(cur_opacity);
+            }, 0.0001);
+        }
         function raise_opacity(cur_opacity)
         {
             setTimeout(function(){
                 if (cur_opacity < 0.7) {
                     set_opacity(cur_opacity);
                 }
-                else return;
+                else{
+                    //set_opacity(0.7);
+                    alert("i'm returning with op=" + parseOpacity());
+                    return;
+                }
                 cur_opacity+=0.005;
                 raise_opacity(cur_opacity);
             }, 0.0001);
