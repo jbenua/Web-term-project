1f7062a531c434cf854e1244704c784ff6c76eb1
Eugenia <harumm.scarumm@gmail.com>
2015-06-23 00:10:09 +0300

    fix

--- scripts/contentView.js
+++ scripts/contentView.js
@@ -1,25 +1,43 @@
         function expanddiv(id){
-            opacity=parseOpacity();
-            raise_opacity(opacity ? opacity : 0);
+            var a=parseOpacity();
+            var opacity = a ? a : 0;
+            id == 'home' ? decrease_opacity(opacity) : raise_opacity(opacity);
             var currentEl=document.getElementById(id);
             for (var i=0, elems = document.getElementsByTagName('p'), len = elems.length; i<len; i++) {
                     removeClass(elems[i], 'active');
             }
             if (currentEl) {
-                currentEl.className += ' active';
+                currentEl.className += 'active';
                 par.style.display="none";
             }
-            else par.style.display="block";
+            else
+            {
+                par.style.display="block";
+            }
         };
         function parseOpacity()
         {
             var temp=glass.style.backgroundColor.split(' ');
             temp=temp[temp.length-1].split(')');
-            return temp[0];
+            return Number(temp[0]);
         }
         function removeClass(element, cls){
             var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
             element.className = element.className.replace(reg,' ');
+        }
+        function decrease_opacity(cur_opacity)
+        {
+            setTimeout(function(){
+                if (cur_opacity > 0) {
+                    set_opacity(cur_opacity);
+                }
+                else{
+                    set_opacity(0);
+                    return;
+                    }
+                cur_opacity-=0.005;
+                decrease_opacity(cur_opacity);
+            }, 0.0001);
         }
         function raise_opacity(cur_opacity)
         {
@@ -27,7 +45,11 @@
                 if (cur_opacity < 0.7) {
                     set_opacity(cur_opacity);
                 }
-                else return;
+                else{
+                    set_opacity(0.7);
+                    alert("i'm returning with op=" + cur_opacity());
+                    return;
+                }
                 cur_opacity+=0.005;
                 raise_opacity(cur_opacity);
             }, 0.0001);
